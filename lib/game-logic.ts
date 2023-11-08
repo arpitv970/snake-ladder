05384b37c736eacefcc21b43d9bedf0ff9892cf3
Arpit Verma <arpitv970@gmail.com>
2023-11-08 21:02:07 +0530

    implemented context api

---
+++ lib/game-logic.ts
@@ -0,0 +1,11 @@
+/*
+ TODO:
+ - start game
+ - set players to 0
+ - iterate through each player
+  - roll the dice
+  - move acc to dice
+  - if stopped at snake / ladder move active player to end point of sanke / ladder
+  - if reached to last tile, active player wins & provide it a rank
+ - end the game by displaying all the players acc to rank
+ */
